d4d2868a9872dafffc955a2c43acc5a1a2bd50f1
krunoo53k <petrik.kpetrik@gmail.com>
2025-02-18 13:07:34 +0100

    feat(repositories): set up repository interfaces and base classes
    This commit introduces the interfaces for VehicleMake and VehicleModel
    repositories and any necessary base classes.  It lays the foundation
    for the data access layer.

---
+++ Service/Data/Repositories/Interfaces/IVehicleModelRepository.cs
@@ -0,0 +1,14 @@
+﻿using Service.Data.Entities;
+
+namespace Service.Data.Repositories.Interfaces;
+
+public interface IVehicleModelRepository
+{
+    Task<VehicleModel> GetByIdAsync(int id);
+    Task<IEnumerable<VehicleModel>> GetAllAsync();
+    Task<IEnumerable<VehicleModel>> GetByMakeIdAsync(int makeId);
+    Task AddAsync(VehicleModel vehicleModel);
+    Task UpdateAsync(VehicleModel vehicleModel);
+    Task DeleteAsync(int id);
+}
+    
