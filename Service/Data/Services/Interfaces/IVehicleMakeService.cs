aa1a825d6085e897351492ca4e49f3d50dcfccba
krunoo53k <petrik.kpetrik@gmail.com>
2025-02-19 14:22:17 +0100

    feat(service): set up service interfaces and base classes

---
+++ Service/Data/Services/Interfaces/IVehicleMakeService.cs
@@ -0,0 +1,12 @@
+﻿using Service.Data.Entities;
+
+namespace Service.Data.Services.Interfaces;
+
+public interface IVehicleMakeService
+{
+    Task<VehicleMake> GetByIdAsync(int id);
+    Task<IEnumerable<VehicleMake>> GetAllAsync();
+    Task CreateAsync(VehicleMake vehicleMake);
+    Task UpdateAsync(VehicleMake vehicleMake);
+    Task DeleteAsync(int id);
+}
---
+++ Service/Data/Services/Interfaces/IVehicleModelService.cs
@@ -0,0 +1,12 @@
+﻿using Service.Data.Entities;
+
+namespace Service.Data.Services.Interfaces;
+
+public interface IVehicleModelService
+{
+    Task<IEnumerable<VehicleModel>> GetAllAsync();
+    Task<VehicleModel> GetByIdAsync(int id);
+    Task CreateAsync(VehicleModel vehicleModel);
+    Task UpdateAsync(VehicleModel vehicleModel);
+    Task DeleteAsync(int id);
+}
