e63dfd1b93328d384c828e8c740332b297057db2
krunoo53k <petrik.kpetrik@gmail.com>
2025-02-20 13:59:23 +0100

    feat(api): add necessary controllers

---
+++ MVC/Controllers/VehicleMakeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Data.Entities;
+using Service.Data.Services.Interfaces;
+
+namespace CarTestWebApp.Controllers;
+
+[Route("api/[controller]")]
+public class VehicleMakeController : Controller
+{
+    private readonly IVehicleMakeService _vehicleMakeService;
+
+    public VehicleMakeController(IVehicleMakeService vehicleMakeService)
+    {
+        _vehicleMakeService = vehicleMakeService;
+    }
+    
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var vehicleMake = await _vehicleMakeService.GetByIdAsync(id);
+        if (vehicleMake is null)
+        {
+            return NotFound();
+        }
+        return Ok(vehicleMake);
+    }
+    
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAll()
+    {
+        var vehicleMakes = await _vehicleMakeService.GetAllAsync();
+        return Ok(vehicleMakes);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] VehicleMake vehicleMake)
+    {
+        await _vehicleMakeService.CreateAsync(vehicleMake);
+        return Ok();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Put([FromBody] VehicleMake vehicleMake)
+    {
+        await _vehicleMakeService.UpdateAsync(vehicleMake);
+        return Ok();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _vehicleMakeService.DeleteAsync(id);
+        return Ok();
+    }
+}
