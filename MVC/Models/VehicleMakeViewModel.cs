088bf70ddaf4e700668dc9e7a6e774eb1b653420
krunoo53k <petrik.kpetrik@gmail.com>
2025-02-20 13:56:52 +0100

    feat(api): add vehicle view models

---
+++ MVC/Models/VehicleMakeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CarTestWebApp.Models;
+
+public class VehicleMakeViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Abrv { get; set; }
+    public virtual IEnumerable<VehicleModelViewModel> Models { get; set; }
+}
