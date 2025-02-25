dbb7d64deba6a11b4531df3afc9330a9477bfca1
krunoo53k <petrik.kpetrik@gmail.com>
2025-02-25 19:20:18 +0100

    feat: remove uneeded property from the view model
    Abbreviation will be a part of the name attribute, as defined in the mapping profile.

--- MVC/Models/VehicleModelViewModel.cs
+++ MVC/Models/VehicleModelViewModel.cs
@@ -4,6 +4,5 @@ public class VehicleModelViewModel
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Abrv { get; set; }
     public int MakeId { get; set; }
 }
