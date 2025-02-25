a6f8ecfb9d6c758b9a3a743b8e5753eb25ca5f66
krunoo53k <petrik.kpetrik@gmail.com>
2025-02-25 19:10:40 +0100

    feat: add automapper mappings
    - Added mappings for exchange between DTOs and Entities
    - Added mappings between ViewModels and DTOs

---
+++ MVC/Mappings/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using CarTestWebApp.Models;
+using Service.Data.DTOs;
+using Service.Data.Entities;
+
+namespace CarTestWebApp.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<VehicleModel, VehicleModelDto>()
+            // FIXME Make might be null. This will create runtime errors.
+            .ForMember(dest => dest.MakeAbrv, opt => opt.MapFrom(src => src.Make.Abrv));
+        CreateMap<VehicleMake, VehicleMakeDto>();
+
+        CreateMap<VehicleMakeDto, VehicleMakeViewModel>();
+        CreateMap<VehicleModelDto, VehicleModelViewModel>()
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => $"{src.Name} ({src.MakeAbrv})"));
+        
+        CreateMap<CreateVehicleMakeDto, VehicleMake>();
+        CreateMap<CreateVehicleModelDto, VehicleModel>();
+        
+    }
+}
