7d498fb853de7a549db12f218eac55658e48d0fa
krunoo53k <petrik.kpetrik@gmail.com>
2025-02-25 16:45:53 +0100

    feat: make class members required and added a default constructor

--- Service/Data/Entities/VehicleMake.cs
+++ Service/Data/Entities/VehicleMake.cs
@@ -3,7 +3,12 @@ namespace Service.Data.Entities;
 public class VehicleMake
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Abrv { get; set; }
+    public required string Name { get; set; }
+    public required string Abrv { get; set; }
     public virtual ICollection<VehicleModel> Models { get; set; }
+    
+    public VehicleMake()
+    {
+        Models = new List<VehicleModel>();
+    }
 }
