d9c1438899371bbfb04bce271f6139e2e34be4ef
krunoo53k <petrik.kpetrik@gmail.com>
2025-02-25 19:21:28 +0100

    feat: remove "Models" collection from the view model
    This commit makes the ViewModel less verbose and query-heavy.

--- MVC/Models/VehicleMakeViewModel.cs
+++ MVC/Models/VehicleMakeViewModel.cs
@@ -5,5 +5,4 @@ public class VehicleMakeViewModel
     public int Id { get; set; }
     public string Name { get; set; }
     public string Abrv { get; set; }
-    public virtual IEnumerable<VehicleModelViewModel> Models { get; set; }
 }
