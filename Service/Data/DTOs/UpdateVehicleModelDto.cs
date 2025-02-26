5fb6f524967ab47f0685fb495444f9d8b431b44e
krunoo53k <petrik.kpetrik@gmail.com>
2025-02-26 15:14:34 +0100

    feat: add vehicle model DTO for update method

---
+++ Service/Data/DTOs/UpdateVehicleModelDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Service.Data.DTOs;
+
+public class UpdateVehicleModelDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int? MakeId { get; set; }
+}
